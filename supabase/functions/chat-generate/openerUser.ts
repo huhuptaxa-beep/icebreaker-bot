2e458c89608839287413bc6332de4275d3ad8e91
huhuptaxa-beep <huhuptaxa@gmail.com>
2026-02-15 23:06:23 +0300

    refactor prompts architecture: move formatting to system, reduce token overhead

--- supabase/functions/chat-generate/openerUser.ts
+++ supabase/functions/chat-generate/openerUser.ts
@@ -2,5 +2,8 @@ export const buildOpenerUserPrompt = (facts: string) => {
   return `
 Описание девушки:
 ${facts}
+
+Создай 3 максимально цепляющих первых сообщения.
+Каждое сообщение должно начинаться с §
 `
 }
